ee10899b5391cb5ea6a486b3a18b9319bb1d7931
V <jeyakuma@8c85902e2eb4.ant.amazon.com>
2018-07-16 01:18:40 +0530

    alpha launch

--- tinto/js/main.js
+++ tinto/js/main.js
@@ -6,7 +6,7 @@ jQuery(document).ready(function ($) {
     /* ==============================================
      Preloader
      =============================================== */
-    $(window).bind("load", function () { // makes sure the whole site is loaded
+    $(window).on("load", function() { // makes sure the whole site is loaded
         $("#status").fadeOut(); // will first fade out the loading animation
         $("#preloader").delay(450).fadeOut("slow"); // will fade out the white DIV that covers the website.
     });
